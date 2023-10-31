23aab646b860b5d0e7d55cbc66abc867e0aaf2b3
tuyizereerneste <tuyizereernest@yahoo.com>
2023-10-31 19:23:21 +0200

    creates a binary tree node

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,10 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -11,14 +14,16 @@
  * @right: Pointer to the right child node
  */
 
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
+typedef struct binary_tree_s binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
-#endif /* _BINARY_TREES_H_ */
+#endif /* BINARY_TREES_H */
