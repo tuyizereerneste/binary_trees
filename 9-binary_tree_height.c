2943ba01a47aa93ae16c419f5168f826e729b0bf
tuyizereerneste <tuyizereernest@yahoo.com>
2023-11-02 18:22:44 +0200

    measures the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,21 +7,12 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height;
-	size_t right_height;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (tree == NULL)
-	{
-		return 0;
-	}
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	if (left_height > right_height)
-	{
-		return left_height + 1;
-	}
-	else
-	{
-		return right_height + 1;
-	}
+		return (0);
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return ((left_height > right_height) ? left_height : right_height);
 }
